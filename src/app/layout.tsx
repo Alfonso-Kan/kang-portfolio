494865ad29e9ea2b139d579d5629445627399987
Alfonso Kan <alf.ks7791@gmail.com>
2024-06-25 13:01:54 -0600

    Add updated CV, custom scrollbar, add icon browser tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+        rel="icon"
+        href="/images/favicon.png"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
